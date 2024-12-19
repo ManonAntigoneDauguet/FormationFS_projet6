fb0a24db9f51f775809114ca0681c35be7b17593
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2024-12-19 17:37:45 +0100

    refactor

--- back/src/main/java/com/openclassrooms/mddapi/controller/PostController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/PostController.java
@@ -5,6 +5,8 @@ import com.openclassrooms.mddapi.common.DTO.apiRequest.PostRequestDTO;
 import com.openclassrooms.mddapi.common.DTO.apiResponse.PostResponseDTO;
 import com.openclassrooms.mddapi.service.CommentService;
 import com.openclassrooms.mddapi.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,24 +24,32 @@ public class PostController {
     }
 
     @GetMapping("post")
+    @Tag(name = "Post")
+    @Operation(summary = "Get all posts", description = "Get a list of all posts")
     public ResponseEntity<Iterable<PostResponseDTO>> getPosts() {
         Iterable<PostResponseDTO> list = postService.getPosts();
         return ResponseEntity.ok(list);
     }
 
     @PostMapping("post")
+    @Tag(name = "Post")
+    @Operation(summary = "Add a post", description = "Add a post")
     public ResponseEntity<String> savePost(@Valid @RequestBody PostRequestDTO postRequestDTO) {
         postService.savePost(postRequestDTO);
         return ResponseEntity.ok("Post correctly sent !");
     }
 
     @GetMapping("post/{id}")
+    @Tag(name = "Post")
+    @Operation(summary = "Get a specific post", description = "Get a specific post")
     public ResponseEntity<PostResponseDTO> getPostById(@PathVariable Long id) {
         PostResponseDTO post = postService.getPostResponseDTOByID(id);
         return ResponseEntity.ok(post);
     }
 
     @PostMapping("post/{id}")
+    @Tag(name = "Post")
+    @Operation(summary = "Add a comment", description = "Add a comment on a specific post")
     public ResponseEntity<String> saveComment(@PathVariable Long id, @Valid @RequestBody CommentRequestDTO commentRequestDTO) {
         commentService.saveComment(id, commentRequestDTO);
         return ResponseEntity.ok("Comment correctly sent !");
--- back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
@@ -4,6 +4,8 @@ import com.openclassrooms.mddapi.common.DTO.apiResponse.PostResponseDTO;
 import com.openclassrooms.mddapi.common.DTO.apiResponse.TopicResponseDTO;
 import com.openclassrooms.mddapi.service.PostService;
 import com.openclassrooms.mddapi.service.TopicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,32 +22,40 @@ public class TopicController {
     }
 
     @GetMapping("topic")
+    @Tag(name = "Topic")
+    @Operation(summary = "Get all topics", description = "Get a list of all topics")
     public ResponseEntity<Iterable<TopicResponseDTO>> getTopics() {
         Iterable<TopicResponseDTO> list = topicService.getTopics();
         return ResponseEntity.ok(list);
     }
 
     @GetMapping("topic/{id}")
+    @Tag(name = "Topic")
+    @Operation(summary = "Get a specific topic", description = "Get a specific topic")
     public ResponseEntity<TopicResponseDTO> getTopicById(@PathVariable Long id) {
         TopicResponseDTO topic = topicService.getTopicResponseDTOByID(id);
         return ResponseEntity.ok(topic);
     }
 
+    @GetMapping("topic/{id}/post")
+    @Tag(name = "Topic")
+    @Operation(summary = "Get concerned posts", description = "Gets a list of all posts about a specific topic")
+    public ResponseEntity<Iterable<PostResponseDTO>> getPostsByTopic(@PathVariable Long id) {
+        Iterable<PostResponseDTO> list = postService.getPostsByTopic(id);
+        return ResponseEntity.ok(list);
+    }
+
     @PostMapping("topic/{id}/subscribe")
+    @Tag(name = "Topic")
     public ResponseEntity<String> subscribe(@PathVariable final Long id) {
         topicService.subscription(id);
         return ResponseEntity.ok("You're now subscribed !");
     }
 
     @DeleteMapping("topic/{id}/subscribe")
+    @Tag(name = "Topic")
     public ResponseEntity<String> unsubscribe(@PathVariable final Long id) {
         topicService.unsubscribe(id);
         return ResponseEntity.ok("You're now unsubscribed !");
-    }
-
-    @GetMapping("topic/{id}/post")
-    public ResponseEntity<Iterable<PostResponseDTO>> getTopicsByTopic(@PathVariable Long id) {
-        Iterable<PostResponseDTO> list = postService.getPostsByTopic(id);
-        return ResponseEntity.ok(list);
     }
 }
