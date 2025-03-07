a555b17eb4ef443638d5b1edc847357985d3c4fb
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2025-03-07 17:05:57 +0100

    fix: correctifs visuels. refactor: simplification du JwtInterceptor.

--- front/src/app/core/interceptors/jwt.interceptor.ts
+++ front/src/app/core/interceptors/jwt.interceptor.ts
@@ -1,22 +1,13 @@
 import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { SessionUserService } from '../services/sessionUser/session-user.service';
-import { switchMap, take } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class JwtInterceptor implements HttpInterceptor {
 
-    constructor(private sessionUserService: SessionUserService) { }
+    constructor() { }
 
     public intercept(request: HttpRequest<any>, next: HttpHandler) {
-        return this.sessionUserService.isLogged$().pipe(
-            take(1),
-            switchMap(isLogged => {
-                if (isLogged) {
-                    request = request.clone({ withCredentials: true });
-                }
-                return next.handle(request);
-            })
-        );
+        request = request.clone({ withCredentials: true });
+        return next.handle(request);
     }
 }
